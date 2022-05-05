b302e2cc7beda27d0ef84feb6d0a285f80e4f524
Schmidt-mat <matisch2010@hotmail.com>
2022-05-05 00:10:40 -0300

    tests OK

--- src/test/java/dds/monedero/model/MonederoTest.java
+++ src/test/java/dds/monedero/model/MonederoTest.java
@@ -7,7 +7,7 @@ import dds.monedero.exceptions.SaldoMenorException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MonederoTest {
   private Cuenta cuenta;
@@ -20,6 +20,7 @@ public class MonederoTest {
   @Test
   void Poner() {
     cuenta.poner(1500);
+    assertEquals(cuenta.getSaldo(),1500);
   }
 
   @Test
@@ -29,9 +30,10 @@ public class MonederoTest {
 
   @Test
   void TresDepositos() {
-    cuenta.poner(1500);
-    cuenta.poner(456);
-    cuenta.poner(1900);
+     cuenta.poner(1500);
+     cuenta.poner(456);
+     cuenta.poner(1900);
+     assertEquals(cuenta.getSaldo(),3856);
   }
 
   @Test
@@ -42,6 +44,13 @@ public class MonederoTest {
           cuenta.poner(1900);
           cuenta.poner(245);
     });
+  }
+
+  @Test
+  void ExtraccionBasica() {
+    cuenta.setSaldo(90);
+    cuenta.sacar(40);
+    assertEquals(cuenta.getSaldo(),50);
   }
 
   @Test
